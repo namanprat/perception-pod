130a09f2dfd94eb9db52cbe03129a30428de44cf
namanprat <59867991+namanprat@users.noreply.github.com>
2025-06-24 01:51:13 +0530

    -

--- src/main.js
+++ src/main.js
@@ -1,24 +1,35 @@
-import './styles.css'
+import './styles/style.css'
+import barba from '@barba/core'
+import webgl from './webgl'
 
-// This is the default Vite main.js for vanilla projects
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button">Count: 0</button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-let count = 0;
-document.getElementById('counter').addEventListener('click', () => {
-  count++;
-  document.getElementById('counter').textContent = `Count: ${count}`;
-});
+// BarbaJS
+barba.init({
+    transitions: [{
+      name: 'default-transition',
+      once() {
+        webgl()
+      },
+      leave() {},
+      enter() {}
+    }],
+    views: [
+        {
+            namespace: 'home',
+            beforeEnter() {
+                sessionStorage.setItem('page', 'home')
+            }
+        },
+        {
+            namespace: 'about',
+            beforeEnter() {
+                sessionStorage.setItem('page', 'about')
+            }
+        },
+        {
+            namespace: 'contact',
+            beforeEnter() {
+                sessionStorage.setItem('page', 'contact')
+            }
+        }
+    ]
+})
