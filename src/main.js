98d4c2cba314a7848ed3be3b7c86b4d997270e1a
namanprat <59867991+namanprat@users.noreply.github.com>
2025-06-24 02:01:30 +0530

    -

--- src/main.js
+++ src/main.js
@@ -1,35 +1,4 @@
-import './styles/style.css'
-import barba from '@barba/core'
+import './styles.css'
 import webgl from './webgl'
 
-// BarbaJS
-barba.init({
-    transitions: [{
-      name: 'default-transition',
-      once() {
-        webgl()
-      },
-      leave() {},
-      enter() {}
-    }],
-    views: [
-        {
-            namespace: 'home',
-            beforeEnter() {
-                sessionStorage.setItem('page', 'home')
-            }
-        },
-        {
-            namespace: 'about',
-            beforeEnter() {
-                sessionStorage.setItem('page', 'about')
-            }
-        },
-        {
-            namespace: 'contact',
-            beforeEnter() {
-                sessionStorage.setItem('page', 'contact')
-            }
-        }
-    ]
-})
+webgl()
