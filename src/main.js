7f5bbbd6b2df002afe0efadec9f7632437234ca5
namanprat <59867991+namanprat@users.noreply.github.com>
2025-06-24 02:41:54 +0530

    -

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import './styles.css'
 import webgl from './webgl'
 import misc from './misc'
+import { Gradient } from './gradient'
+
 
 
 webgl()
 misc()
+const gradient = new Gradient()
+gradient.initGradient('#gradient-canvas')
