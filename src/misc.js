aa821bb59f4d5333a6376da58023bba291b57788
namanprat <59867991+namanprat@users.noreply.github.com>
2025-07-19 11:51:28 +0530

    -

--- src/misc.js
+++ src/misc.js
@@ -1,5 +1,37 @@
 function misc()
 {
+  const start = performance.now();
+const imgLoad = new imagesLoaded("body", { background: true }, onImagesLoaded);
+const numImages = imgLoad.images.length;
+imgLoad.on("progress", function (instance, image) {
+  var result = image.isLoaded ? "loaded" : "broken";
+  console.log(
+    `image ${instance.progressedCount} out of ${numImages} is ${result}`
+  );
+  const progress = instance.progressedCount / numImages;
+  // gsap loader animation shows progress of images loading
+  gsap.to(".progress-bar", {
+    scaleX: progress,
+  });
+});
+function onImagesLoaded() {
+  const end = performance.now();
+  console.log(
+    `Time taken to load ${numImages} images: ${Math.round(end - start)}ms`
+  );
+  // Calculate remaining time to ensure loader is displayed for a minimum time
+  const MIN_TIME = 1000;
+  const duration = end - start;
+  const remainingTime = Math.max(MIN_TIME - duration, 0);
+  gsap.to(".preloader_wrap", {
+    delay: remainingTime / 1000,
+    yPercent: -100,
+    onComplete: () => {
+      // re-enable scrolling
+      gsap.set("body", { overflow: "auto" });
+    },
+  });
+}
     document.addEventListener("DOMContentLoaded", function () {
         gsap.registerPlugin(ScrollTrigger, SplitText, ScrollToPlugin, Flip);
       
